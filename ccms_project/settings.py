3e83337396cf3358eb674d0f05c588b2845396b1
Nikita-1306 <dhanawadenikita13@gmail.com>
2025-09-22 23:21:56 +0530

    added allowed host0

--- ccms_project/settings.py
+++ ccms_project/settings.py
@@ -3,7 +3,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dev-secret-key-change-this'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ccms-project.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 INSTALLED_APPS = [
     'custom_admin',
     'django.contrib.admin',
